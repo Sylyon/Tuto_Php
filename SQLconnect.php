c0c1b9fb6f76223622f2c1d6368b054bc52c7881
Sylyon <bastien.drouot@ensta-bretagne.org>
2015-06-01 18:01:29 +0200

    fist stape with mySQL ( and POD)

--- SQLconnect.php
+++ SQLconnect.php
@@ -7,4 +7,51 @@
 	{
 		die('Erreur : '.$e -> getMessage());
 	}
+//Request 1
+	$allGames = $myBDD -> query('SELECT * FROM jeux_video');
+	?>
+	<table>
+		<tr>
+			<td><strong>nom</strong></td>
+			<td><strong>console</strong></td>
+			<td><strong>commentaires</strong></td>
+		</tr>
+	<?php
+	while ($aGame = $allGames -> fetch())
+	{
+		?>
+		<tr>
+			<td><?php echo $aGame['nom']; ?></td>
+			<td><?php echo $aGame['console']; ?></td>
+			<td><?php echo $aGame['commentaires']; ?></td>
+		</tr>
+		<?php
+	}
+	echo '</table>';
+	$allGames -> closeCursor();
+//END Request 1
+//Request 2
+	$allGames = $myBDD -> query('SELECT nom, console, prix FROM jeux_video WHERE console="PS2" AND prix < 35 ORDER BY prix DESC LIMIT 0,5 ');
+	?>
+	<table>
+		<tr>
+			<td><strong>nom</strong></td>
+			<td><strong>console</strong></td>
+			<td><strong>prix</strong></td>
+		</tr>
+	<?php
+	while ($aGame = $allGames -> fetch())
+	{
+		?>
+		<tr>
+			<td><?php echo $aGame['nom']; ?></td>
+			<td><?php echo $aGame['console']; ?></td>
+			<td><?php echo $aGame['prix']; ?></td>
+		</tr>
+		<?php
+	}
+	echo '</table>';
+	$allGames -> closeCursor();
+//END Request 2
+
 ?>
